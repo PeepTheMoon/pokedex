4f5f4eaaa8bb47a2322644d9e9c11061b3894812
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-04-29 15:23:29 -0700

    wireframe added to public folder

--- src/App.js
+++ src/App.js
@@ -10,3 +10,4 @@ export default class App extends Component {
     )
   }
 }
+// const fetchedData = await request.get('https://alchemy-pokedex.herokuapp.com/api/pokedex/5cef3501ef6005a77cd4fd33') ;
